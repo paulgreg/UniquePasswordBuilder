5176c9014877a2a9054144a76b89add3fc812d4c
Philippe Miossec <pmiossec@gmail.com>
2018-02-10 14:32:16 +0100

    fix gulp crashing often
    All tasks writing in 'dist' folder should wait that 'clean' task is finished

--- gulpfile.js
+++ gulpfile.js
@@ -25,21 +25,21 @@ gulp.task('clean-dist', function(cb){
 
 gulp.task('clean', ['clean-dist']);
 
-gulp.task('bookmarklet', function() {
+gulp.task('bookmarklet', ['clean'], function() {
   return gulp.src(paths.bookmarklet)
     .pipe(uglify())
     .pipe(concat('upb.min.js'))
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('index', function() {
+gulp.task('index', ['clean'], function() {
   return gulp.src(paths.index)
     .pipe(uglify())
     .pipe(concat('upb-main.min.js'))
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('argon2', function() {
+gulp.task('argon2', ['clean'], function() {
     return gulp.src('node_modules/argon2-browser/dist/argon2-asm.min.js')
       .pipe(gulp.dest('dist'))
       .pipe(gulp.dest('addon'))
@@ -48,26 +48,26 @@ gulp.task('argon2', function() {
 
     gulp.task('font-awesome', ['font-awesome-css', 'font-awesome-fonts']);
 
-    gulp.task('font-awesome-css', function() {
+    gulp.task('font-awesome-css', ['clean'], function() {
         return gulp.src('node_modules/font-awesome/css/*')
             .pipe(gulp.dest('dist/font-awesome/css'))
             .pipe(gulp.dest('addon/font-awesome/css'));
         });
 
-    gulp.task('font-awesome-fonts', function() {
+    gulp.task('font-awesome-fonts', ['clean'], function() {
         return gulp.src('node_modules/font-awesome/fonts/*')
             .pipe(gulp.dest('dist/font-awesome/fonts'))
             .pipe(gulp.dest('addon/font-awesome/fonts'))
             ;
         });
 
-gulp.task('html', function() {
+gulp.task('html', ['clean'], function() {
     return gulp.src(['index.html', 'bookmark_scrypt_test.html', 'bookmark_argon2_test.html'])
     .pipe(gulp.dest('dist'))
     ;
     });
 
-gulp.task('assets', function() {
+gulp.task('assets', ['clean'], function() {
     return gulp.src('assets\\*')
     .pipe(gulp.dest('dist'))
     ;
@@ -80,6 +80,6 @@ gulp.task('addon-copy-js', ['index'], function() {
 
 gulp.task('addon', ['addon-copy-js']);
 
-gulp.task('page', ['clean', 'html', 'index', 'assets']);
+gulp.task('page', ['html', 'index', 'assets']);
 
 gulp.task('default', ['page', 'bookmarklet', 'addon', 'argon2', 'font-awesome']);

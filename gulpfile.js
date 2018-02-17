55d45811a05531c9972c318303450e5b8604bc3e
Philippe Miossec <pmiossec@gmail.com>
2018-02-17 23:11:56 +0100

    Format gulpfile.js to stay consistent

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,6 @@ var paths = {
     ]
 };
 
-
 gulp.task('clean-dist', function(cb){
   rimraf('dist/', cb);
 });
@@ -42,35 +41,31 @@ gulp.task('index', function() {
 gulp.task('argon2', function() {
     return gulp.src('node_modules/argon2-browser/dist/argon2-asm.min.js')
       .pipe(gulp.dest('dist'))
-      .pipe(gulp.dest('addon'))
-      ;
-    });
+      .pipe(gulp.dest('addon'));
+});
 
-    gulp.task('font-awesome', ['font-awesome-css', 'font-awesome-fonts']);
+gulp.task('font-awesome', ['font-awesome-css', 'font-awesome-fonts']);
 
-    gulp.task('font-awesome-css', function() {
-        return gulp.src('node_modules/font-awesome/css/*')
-            .pipe(gulp.dest('dist/font-awesome/css'))
-            .pipe(gulp.dest('addon/font-awesome/css'));
-        });
+gulp.task('font-awesome-css', function() {
+    return gulp.src('node_modules/font-awesome/css/*')
+        .pipe(gulp.dest('dist/font-awesome/css'))
+        .pipe(gulp.dest('addon/font-awesome/css'));
+});
 
-    gulp.task('font-awesome-fonts', function() {
-        return gulp.src('node_modules/font-awesome/fonts/*')
-            .pipe(gulp.dest('dist/font-awesome/fonts'))
-            .pipe(gulp.dest('addon/font-awesome/fonts'))
-            ;
-        });
+gulp.task('font-awesome-fonts', function() {
+    return gulp.src('node_modules/font-awesome/fonts/*')
+        .pipe(gulp.dest('dist/font-awesome/fonts'))
+        .pipe(gulp.dest('addon/font-awesome/fonts'));
+});
 
 gulp.task('html', function() {
     return gulp.src(['index.html', 'bookmark_scrypt_test.html', 'bookmark_argon2_test.html'])
-    .pipe(gulp.dest('dist'))
-    ;
-    });
+    .pipe(gulp.dest('dist'));
+});
 
 gulp.task('assets', function() {
     return gulp.src('assets/*')
-    .pipe(gulp.dest('dist'))
-    ;
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('addon-copy-js', ['index'], function() {

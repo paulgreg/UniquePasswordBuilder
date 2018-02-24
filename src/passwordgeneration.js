c11f8ee85c4b5b03d84714908a27bf3133b029f9
Philippe Miossec <pmiossec@gmail.com>
2018-02-24 15:27:02 +0100

    make `availableChars`a string

--- src/passwordgeneration.js
+++ src/passwordgeneration.js
@@ -3,15 +3,9 @@
     var passwordLength = 16;
     upb.SCRYPT = 'scrypt';
     upb.ARGON2 = 'argon2';
+    var availableChars = '!$+-=_.:;,?#%&()[]' + '0123456789' + 'abcdefghijklmnopqrstuvwxyz' + 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
     upb.makeHashHumanReadable = function(array) {
-        var availableChars = [
-            '!','$','+','-','=','_','.',':',';',',','?','#','%','&','(',')','[',']',
-            '0','1','2','3','4','5','6','7','8','9',
-            'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
-            'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
-        ];
-
         var password = '';
         for(var i = 0; i < array.length; i+=2) {
             var v = array[i] + array[i+1];

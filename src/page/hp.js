d6cdc80ad78c7526ee57f4adfd0bfc47fcab78bd
Grégory Paul <paulgreg@gmail.com>
2019-02-23 17:34:22 +0100

    Save previous URLs to avoid typing it on mobile

--- src/page/hp.js
+++ src/page/hp.js
@@ -25,6 +25,7 @@
         if (localStorage.usersalt) usersaltInput.value = localStorage.usersalt;
         if(localStorage.options === 'true') optionsDiv.classList.remove('hidden');
         changeAlgorithm();
+        renderDomains()
     }
 
     save = function() {
@@ -50,6 +51,73 @@
 
     displayInfosLink.addEventListener('click', displayInfos, false);
     document.getElementById('copyBookmarkletToClipboard').addEventListener('click', copyBookmarkletToClipboard, false);
+
+
+    var domainRegex = /^https?:\/\/(?:[^\/?]+)/i
+    var url = document.querySelector('#url')
+    url.addEventListener('change', function (e) {
+        if (domainRegex.test(url.value)) {
+            var domain = domainRegex.exec(url.value)
+            saveDomain(domain[0])
+            renderDomains()
+        }
+    }, false)
+
+    function getDomains () {
+        var domains = localStorage.domains
+        return domains = domains ? JSON.parse(domains) : []
+    }
+
+    function saveDomain (domain) {
+        var domains = getDomains()
+        if (domains.indexOf(domain) !== -1) return
+        domains.push(domain)
+        localStorage.domains = JSON.stringify(domains)
+    }
+
+    function empty(list) {
+        while (list.firstChild) {
+            list.removeChild(list.firstChild)
+        }
+    }
+
+    function selectDomain (domain) {
+        url.value = domain
+    }
+
+    function removeDomain (domain) {
+        var domains = getDomains()
+        var idx = domains.indexOf(domain)
+        domains.splice(idx, 1)
+        localStorage.domains = JSON.stringify(domains)
+        renderDomains()
+    }
+
+    var domainsTitle = document.querySelector('#domainsTitle')
+    var list = document.querySelector('#domains')
+    function renderDomains () {
+        empty(list)
+        var domains = getDomains()
+        var isEmpty = domains.length === 0
+        domainsTitle.style.display = isEmpty ? 'none' : 'block'
+        if (isEmpty) return
+        domains.map(function (domain) {
+            if (domain === '') return
+            var li = document.createElement('li')
+            var aDomain = document.createElement('a')
+            var aRemove = document.createElement('a')
+            aDomain.innerText = domain
+            aDomain.className = 'domain'
+            aRemove.innerText = 'remove'
+            aRemove.className = 'remove'
+            aDomain.addEventListener('click', selectDomain.bind(null, domain), false)
+            aRemove.addEventListener('click', removeDomain.bind(null, domain), false)
+            li.appendChild(aDomain)
+            li.appendChild(aRemove)
+            list.appendChild(li)
+        })
+    }
+
 
     load();
 
